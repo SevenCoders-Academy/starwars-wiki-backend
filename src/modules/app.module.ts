478aec682f62ffa3c87190bcab2f04c1772c72d2
Gabriel Ávia Rangel <gabriel.avila.rangel@gmail.com>
2021-07-20 11:10:24 -0300

    ref: database config based on ENV

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -6,15 +6,24 @@ import { CharacterModule } from './character';
 
 config();
 
+const DB_CONFIG =
+  process.env.ENV === 'LOCAL'
+    ? {
+        host: String(process.env.DB_HOST),
+        port: Number(process.env.DB_PORT),
+        username: String(process.env.DB_USER),
+        password: String(process.env.DB_PASS),
+        database: String(process.env.DB_NAME),
+      }
+    : {
+        url: String(process.env.DATABASE_URL),
+      };
+
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: String(process.env.DB_HOST),
-      port: Number(process.env.DB_PORT),
-      username: String(process.env.DB_USER),
-      password: String(process.env.DB_PASS),
-      database: String(process.env.DB_NAME),
+      ...DB_CONFIG,
       entities: [__dirname + '../../**/*.entity{.ts,.js}'],
       synchronize: true,
       logging: true,
