be6a52ecf538f8c925471ed4ff41673a05ff6ab0
Gabriel Ávia Rangel <gabriel.avila.rangel@gmail.com>
2021-07-20 11:20:55 -0300

    fix: remove unnecessary env config and add SSL to connect db

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -5,27 +5,18 @@ import { FilmeModule } from './film';
 import { CharacterModule } from './character';
 
 config();
-
-const DB_CONFIG =
-  process.env.ENV === 'LOCAL'
-    ? {
-        host: String(process.env.DB_HOST),
-        port: Number(process.env.DB_PORT),
-        username: String(process.env.DB_USER),
-        password: String(process.env.DB_PASS),
-        database: String(process.env.DB_NAME),
-      }
-    : {
-        url: String(process.env.DATABASE_URL),
-      };
-
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      ...DB_CONFIG,
+      host: String(process.env.DB_HOST),
+      port: Number(process.env.DB_PORT),
+      username: String(process.env.DB_USER),
+      password: String(process.env.DB_PASS),
+      database: String(process.env.DB_NAME),
       entities: [__dirname + '../../**/*.entity{.ts,.js}'],
       synchronize: true,
+      ssl: true,
       logging: true,
     }),
     FilmeModule,
